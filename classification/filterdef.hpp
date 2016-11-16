4387d9c65450f6f4b8f659145a43ffaa4255d375
Kenta Nakajima <knknkn1162@gmail.com>
2016-11-16 16:27:30 +0900

    comment on filterdef.hpp

--- classification/filterdef.hpp
+++ classification/filterdef.hpp
@@ -1,3 +1,12 @@
+/**
+* @file   filterdef.hpp
+* @brief separate definition with declaration. see also filter.hpp.
+* @author   Kenta Nakajima
+* @date   2016.11.16
+* @version   1.0
+* @bug   nothing, I think.
+* @warning  this code may include some wrong descriptions
+*/
 template <typename ForwardIterator, typename UnaryPredicate>
 int count_iterators(ForwardIterator first, ForwardIterator last, UnaryPredicate pred) {
 	int res = 0;
